c3c2cf36664725a6cc3498e8c9b0dc5e7918c88e
N1jel PC <vanojefferson@gmail.com>
2026-03-25 22:56:16 +0300

    Initial commit

---
+++ src/main/java/by/bsu/n1jel/pc/assembler/PcAssemblerApplication.java
@@ -0,0 +1,13 @@
+package by.bsu.n1jel.pc.assembler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcAssemblerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcAssemblerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/bsu/n1jel/pc/assembler/PcAssemblerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.n1jel.pc.assembler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcAssemblerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
