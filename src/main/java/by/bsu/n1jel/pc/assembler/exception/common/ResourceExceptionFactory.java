d5d756d760dfa9e35e6ad6b88d86514e1f4c2380
N1jel PC <vanojefferson@gmail.com>
2026-05-22 00:54:45 +0300

    Fix resource exception naming faults

--- src/main/java/by/bsu/n1jel/pc/assembler/exception/common/ResourceExceptionFactory.java
+++ src/main/java/by/bsu/n1jel/pc/assembler/exception/common/ResourceExceptionFactory.java
@@ -3,7 +3,7 @@ package by.bsu.n1jel.pc.assembler.exception.common;
 public class ResourceExceptionFactory {
 
     public static ResourceNotFoundException specificationTypeNotFoundException(Long id){
-        return new ResourceNotFoundException(id, "Specification objectType id = %s not found".formatted(id));
+        return new ResourceNotFoundException(id, "Specification id = %s not found".formatted(id));
     }
 
     public static ResourceNotFoundException buildNotFoundException(Long id){
@@ -19,7 +19,7 @@ public class ResourceExceptionFactory {
     }
 
     public static ResourceNotFoundException componentTypeNotFoundException(Long id){
-        return new ResourceNotFoundException(id, "Component objectType id = %s not found".formatted(id));
+        return new ResourceNotFoundException(id, "Component id = %s not found".formatted(id));
     }
 
     public static ResourceNotFoundException producerNotFoundException(Long id){
