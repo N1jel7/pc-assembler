0f0dae3ad6d06f0d3fdbd25da568793b03aa567d
N1jel PC <vanojefferson@gmail.com>
2026-05-22 21:54:36 +0300

    Image rest controler swagger configuration

--- src/main/java/by/bsu/n1jel/pc/assembler/controller/rest/RestImageController.java
+++ src/main/java/by/bsu/n1jel/pc/assembler/controller/rest/RestImageController.java
@@ -5,10 +5,13 @@ import by.bsu.n1jel.pc.assembler.dto.request.image.LoadImageRequestDto;
 import by.bsu.n1jel.pc.assembler.dto.request.image.UploadImageRequestDto;
 import by.bsu.n1jel.pc.assembler.dto.response.ImageInfoResponseDto;
 import by.bsu.n1jel.pc.assembler.service.api.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Build&Partition", description = "Build and build partition rest controller")
 @RestController
 @RequestMapping("api/v1/images")
 @RequiredArgsConstructor
@@ -16,16 +19,28 @@ public class RestImageController {
 
     private final ImageService imageService;
 
+    @Operation(
+            summary = "Get image by id",
+            description = "Return encoded string"
+    )
     @GetMapping("/{id}")
     public String get(@PathVariable Long id) {
         return imageService.load(new LoadImageRequestDto(id));
     }
 
+    @Operation(
+            summary = "Delete by id",
+            description = "Deleting image by id and after success returns info about deleted image"
+    )
     @DeleteMapping("/{id}")
     public ImageInfoResponseDto delete(@PathVariable Long id) {
         return imageService.delete(new DeleteImageRequestDto(id));
     }
 
+    @Operation(
+            summary = "Upload",
+            description = "Uploading image by request dto"
+    )
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ImageInfoResponseDto upload(@ModelAttribute UploadImageRequestDto uploadImageRequestDto) {
         return imageService.upload(uploadImageRequestDto);
